838311b0630a9cef1c321fa5c25b46e2570c275e
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2024-01-26 15:57:34 +0300

    added new excepiton classes

--- src/main/java/com/tfunsal/eventhub/exception/UnauthorizedClubAccessException.java
+++ src/main/java/com/tfunsal/eventhub/exception/UnauthorizedClubAccessException.java
@@ -3,9 +3,9 @@ package com.tfunsal.eventhub.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class ClubNotFoundException extends RuntimeException{
-    public ClubNotFoundException(String message) {
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnauthorizedClubAccessException extends RuntimeException {
+    public UnauthorizedClubAccessException(String message) {
         super(message);
     }
 }
